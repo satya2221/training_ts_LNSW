7effee9356b340c3cec63acb57c1c1ff6f038624
satya2221 <satyaghifari12@gmail.com>
2025-09-23 12:59:15 +0700

    day 1: enum & interface

--- ts-inventory-cli/src/index.ts
+++ ts-inventory-cli/src/index.ts
@@ -9,3 +9,34 @@ console.log(`Selamat datang di ${inventoryName}!`);
 
 // Coba buat kesalahan!
 // totalItems = "lima ratus"; // TypeScript akan langsung error di sini!
+
+enum ProductCategory {
+  ELECTRONICS = "ELEKTRONIK",
+  ACCESSORIES = "AKSESORIS",
+  GADGETS = "GAJET",
+}
+
+
+interface Product {
+  id: number;
+  name: string;
+  brand: string;
+  price: number;
+  quantity: number;
+  category?: ProductCategory;
+}
+
+const firstProduct: Product = {
+  id: 1,
+  name: "Smart TV 55 inch",
+  brand: "Samsung",
+  price: 7500000,
+  quantity: 10,
+  category: ProductCategory.ELECTRONICS
+};
+
+function printProductInfo(product: Product): void {
+  console.log(`- ${product.name} (${product.brand}), Harga: Rp${product.price}, Stok: ${product.quantity}`);
+}
+
+printProductInfo(firstProduct);
